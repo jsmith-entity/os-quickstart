084cd2892dd1c85bf4ea190d874fb6c7a2060828
jsmith.entity <jsmith973@protonmail.com>
2025-05-14 00:41:36 +0800

    Initial Commit - current files

---
+++ packages/install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Desktop Environment / Utils
+sudo pacman -S --noconfirm \
+	hyprland \
+	hyprpaper \
+	waybar \
+	rofi \
+	sddm
+
+# Fonts
+sudo pacman -S --noconfirm \
+	ttf-fira-mono \
+	ttf-liberation \
+	ttf-ms-fonts \
+	ttf-noto-nerd
+
+# Terminal and Coding
+sudo pacman -S --noconfirm \
+	alacritty \
+	fzf \
+	git \
+	neovim \
+	ripgrep \
+	tmux \
+	zsh
+
+# Terminal Utils
+sudo pacman -S --noconfirm \
+	base-devel \
+	gnupg \
+	grep \
+	openssh \
+	pass \
+	unzip \
+	which \
+	xdg-desktop \
+	xdg-utils \
+	zip
+
+# Coding Utilities
+sudo pacman -S --noconfirm \
+	gcc \
+	npm
+
+# Miscellaneous
+sudo pacman -S --noconfirm \
+	curl \
+	firefox \
+	libreoffice \
+	pipewire pipewire-pulse \
+	pavucontrol
